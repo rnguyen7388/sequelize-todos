0b3c13cae579b1cb90b4d32c1b806b71c630be4c
Ray <rnguyen7388@gmail.com>
2020-08-06 21:20:49 -0500

    updated validation

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,10 @@ module.exports = function(sequelize, DataTypes) {
   const Todo = sequelize.define("Todo", {
     text: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
     },
     complete: {
       type: DataTypes.BOOLEAN,
