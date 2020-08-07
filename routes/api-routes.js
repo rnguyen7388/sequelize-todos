8f7dc6fabba46d3fc9a94b6b9859d9de7d70eef9
Ray <rnguyen7388@gmail.com>
2020-08-06 20:49:01 -0500

    Adds DELETE and POST routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,10 +9,9 @@ const db = require('../models')
 module.exports = function(app) {
 
   // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
-    db.Todo.findAll().then((data) => {
-      res.json(data)
-    })
+  app.get("/api/todos", async function(req, res) {
+    const data = await db.Todo.findAll()
+    res.json(data)
   });
 
   // POST route for saving a new todo. We can create a todo using the data on req.body
@@ -25,11 +24,21 @@ module.exports = function(app) {
   // DELETE route for deleting todos. We can access the ID of the todo to delete in
   // req.params.id
   app.delete("/api/todos/:id", function(req, res) {
-    res.end()
+    const id = (req.params.id)
+    db.Todo.destroy({
+      where: { id }
+    })
+    .then(data => res.json(data))
+    .catch(err => { throw err })
   });
 
   // PUT route for updating todos. We can access the updated todo in req.body
-  app.put("/api/todos", function(req, res) {
-    res.end()
+  app.put("/api/todos", async function(req, res) {
+    const { id, text, complete } = req.body
+    const data = await db.Todo.update(
+      { text, complete },
+      { where: { id } }
+    )
+    res.json(data)
   });
 };
